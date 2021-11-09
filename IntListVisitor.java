57a421dbb467d332a9548bb4f0d8531947414191
Corky Cartwright <corky.cartwright@gmail.com>
2021-11-08 22:28:31 -0600

    Delete IntListVisitor.java
    Not applicable to Assignment 8.

--- IntListVisitor.java
+++
@@ -1,5 +0,0 @@
-/** Visitor interface for the IntList type. */
-interface IntListVisitor {
-  abstract Object forEmptyIntList(EmptyIntList host);
-  abstract Object forConsIntList(ConsIntList host);
-}
