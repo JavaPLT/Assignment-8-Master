d4998cf55b82927a7eb81b1147f5efe86c8aef06
Corky Cartwright <corky.cartwright@gmail.com>
2021-11-08 22:28:58 -0600

    Delete LengthVisitor.java
    Not applicable to Assignment 8.

--- LengthVisitor.java
+++
@@ -1,9 +0,0 @@
-/** IntListVisitor class that implements the length() method on IntLists. */
- class LengthVisitor implements IntListVisitor {
-  /* Singleton binding for LengthVisior because the method takes no parameters */
-  public static final LengthVisitor ONLY = new LengthVisitor();  
-  private LengthVisitor() { }
-  
-  public Object forEmptyIntList(EmptyIntList el) { return 0; }
-  public Object forConsIntList(ConsIntList cil) { return 1 + (Integer) cil.rest().visit(this); }
-}
